35f9f5c9bda7094fdcf6b7945203a10bfe6c102b
jfongue <fongue.jeremy@gmail.com>
2026-05-08 15:49:37 +0200

    fix(metro): désactiver unstable_enablePackageExports pour fix eas-cli metro-resolver

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Fix: désactive la résolution package.json:exports (activée par défaut depuis Expo SDK 53)
+// qui provoque "Cannot find module './utils/paths'" dans metro-resolver lors des EAS builds.
+config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
